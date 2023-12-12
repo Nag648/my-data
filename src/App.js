6d8753f93bd1d9f116f6dc82e175e993523bf46e
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-12-12 16:58:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 function App() {
-  const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+    const [editingTodo, setEditingTodo] = useState(null);
 
-  
-  const addTodo = async () => {
-    const data ={
-      text: newTodo,
-      completed: false
-    }
-    try {
-      const response = await axios.post('http://localhost:6y000/todo', data);
-        console.log(response)
-    //    fetchTodos()
-    //   setTodos([...todos, response.data]);
-      setNewTodo('');
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    useEffect(() => {
 
 
-  return (
-    <div>
-      <h1>Todo App</h1>
-      {/* <ul>
-        {todos.map(todo => (
-          <li key={todo._id}>{todo.text}</li>
-        ))}
-      </ul> */}
-      <div>
-        <input type="text" value={newTodo} onChange={(e) => {setNewTodo(e.target.value)} }/>
-       
-        <button onClick={addTodo}>Add Todo</button>
-      </div>
-    </div>
-  );
+        fetchTodos();
+    },[]);
+    const fetchTodos = async () => {
+        try {
+           const respose = await axios.get('http://localhost:5000/todos');
+           console.log(respose.data)
+           setTodos(response.data); 
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+    const addTodo = async () => {
+       const data = {
+        text: newTodo,
+        completed: false
+       }
+       try {
+        const response = await axios.post('http://localhost:5000/todos',data);
+        fetchTodos()
+        // setTodos([...todos,response.data]);
+        setNewTodo(''); 
+       } catch (error){
+        console.error(error);
+       }
+    };
+
+    const deleteRodo = async (id) => {
+        try {
+            await axios.delete('http://localhost:5000/Todos/${id}');
+
+
+            fetchTodos()
+
+        } catch (error) {
+            console.error(error);
+
+            
+        }
+    } ;
+    const startEditing = (id) => {
+        setEditingTodo(id);
+    };
+
+    const saveEdit = async (id,newText) =>{
+     try {
+        await axios.put(`http://localhost:5000/todos/${id}`
+       ,{text:newText});
+
+        fetchTodos()
+        setEditingTodo(null);
+     } catch (error) {
+        console.error(error);
+     }  
+    };
+    console.log("editingTodo",editingTodo)
+
+
+    return (
+        <div>
+            <h1>Todo App</h1>
+            <ul>
+             {todos.map(todo =>(
+                <li key={todo._id}>
+                    {editingTodo === todo._id ? (
+                        <div>
+                            <input
+                            type="text"
+                            value={todo.text}
+
+                            onChange={(e)=> setTodos(todos.map(t =>(t._id === editingTodo ? {...t,text: e.target.value}:t)))}
+                            />
+
+
+
+
+                            <button onClick={()=>saveEdit(editingTodo,todo.text)}>Save</button>
+                        </div>
+                    ) : (
+                        <div>
+                            {todo.text}
+                            <button onClick={()=> deleteTodo(todo._id)}>Delete</button>
+                            <button onClick={() =>startEditing(todo._id)}>Edit</button>
+
+
+                        </div>
+                    )}
+                </li>
+             ))}   
+            </ul>
+            <div>
+                <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+                <button onClick={addTodo}>Add Todo</button>
+            </div>
+        </div>
+    )
 }
+
 
 export default App;
